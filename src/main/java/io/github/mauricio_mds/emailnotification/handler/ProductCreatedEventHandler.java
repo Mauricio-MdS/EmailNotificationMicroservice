aa04e20fa496e09967790a0447f4347d51855107
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-10-08 20:14:46 -0300

    ProductCreatedEventHandler logs productId

--- src/main/java/io/github/mauricio_mds/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/io/github/mauricio_mds/emailnotification/handler/ProductCreatedEventHandler.java
@@ -15,6 +15,7 @@ public class ProductCreatedEventHandler {
 
     @KafkaHandler
     public  void handle(ProductCreatedEvent productCreatedEvent) {
-        LOGGER.info("Received a new event: " + productCreatedEvent.getTitle());
+        LOGGER.info("Received a new event: " + productCreatedEvent.getTitle() +
+                " with productId: " + productCreatedEvent.getProductId());
     }
 }
