8dcb73be2fbc1bebd3a124c9c4c2c271727078bc
Mauricio-MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-10-09 16:33:59 -0300

    Persist event to database

--- src/main/java/io/github/mauricio_mds/emailnotification/handler/ProductCreatedEventHandler.java
+++ src/main/java/io/github/mauricio_mds/emailnotification/handler/ProductCreatedEventHandler.java
@@ -1,21 +1,50 @@
 package io.github.mauricio_mds.emailnotification.handler;
 
 import io.github.mauricio_mds.core.ProductCreatedEvent;
+import io.github.mauricio_mds.emailnotification.error.NotRetryableException;
+import io.github.mauricio_mds.emailnotification.io.ProcessedEventEntity;
+import io.github.mauricio_mds.emailnotification.io.ProcessedEventRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @KafkaListener(topics="product-created-events-topic")
 public class ProductCreatedEventHandler {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private ProcessedEventRepository processedEventRepository;
 
     @KafkaHandler
-    public  void handle(ProductCreatedEvent productCreatedEvent) {
+    @Transactional
+    public  void handle(
+            @Payload ProductCreatedEvent productCreatedEvent,
+            @Header("messageId") String messageId,
+            @Header(KafkaHeaders.RECEIVED_KEY) String messageKey) {
         LOGGER.info("Received a new event: " + productCreatedEvent.getTitle() +
                 " with productId: " + productCreatedEvent.getProductId());
+
+        // Check if this message was already processed before
+        ProcessedEventEntity existingRecord = processedEventRepository.findByMessageId(messageId);
+        if (existingRecord != null) {
+            LOGGER.info("Found a duplicate message id: {}", existingRecord.getMessageId());
+            return;
+        }
+
+        // Save a unique message id in a database table
+        try {
+            processedEventRepository.save(new ProcessedEventEntity(messageId, productCreatedEvent.getProductId()));
+        } catch (DataIntegrityViolationException e) {
+            throw new NotRetryableException(e);
+        }
     }
 }
